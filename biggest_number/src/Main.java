69beda29341a38bef163aa95b69ec46209cd6bfa
Doğanay Tay <116439695+doganaytay1097@users.noreply.github.com>
2023-08-03 11:21:17 +0300

    Biggest Number

---
+++ biggest_number/src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 17;
+        int number2 = 13;
+        int number3 = 15;
+        int biggest = number1;
+        if (biggest<number2){
+            biggest = number2;
+
+        }
+        if (biggest<number3){
+            biggest = number3;
+
+        }
+        System.out.println(biggest);
+    }
+}
